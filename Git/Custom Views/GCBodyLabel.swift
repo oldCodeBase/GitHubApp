b898bce1187b92428769f275a4e956be324d9676
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-14 14:35:09 +0300

    Created custom BodyLabel

---
+++ Git/Custom Views/GCBodyLabel.swift
@@ -0,0 +1,38 @@
+//
+//  GCBodyLabel.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 14/01/2021.
+//
+
+import UIKit
+
+class GCBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment  = textAlignment
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    
+    private func configure() {
+        textColor                                   = .label
+        font                                        = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth                   = true
+        minimumScaleFactor                          = 0.75
+        lineBreakMode                               = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints   = false
+    }
+    
+}
