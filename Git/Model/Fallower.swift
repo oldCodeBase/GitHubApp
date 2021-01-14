a450a0a53219c06c2b65f6a1e13f0adb9a5cb2b6
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-14 19:11:08 +0300

    Made two models

---
+++ Git/Model/Fallower.swift
@@ -0,0 +1,13 @@
+//
+//  Fallower.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 14/01/2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
