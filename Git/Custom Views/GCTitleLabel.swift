9c72f064a19f4848a7e26ce561ecd3adffc3333e
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-14 14:34:49 +0300

    Created custom TitleLabel

---
+++ Git/Custom Views/GCTitleLabel.swift
@@ -0,0 +1,38 @@
+//
+//  GCLabel.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 14/01/2021.
+//
+
+import UIKit
+
+class GCTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment  = textAlignment
+        self.font           = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    
+    private func configure() {
+        textColor                                   = .label
+        adjustsFontSizeToFitWidth                   = true
+        minimumScaleFactor                          = 0.9
+        lineBreakMode                               = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints   = false
+    }
+
+}
