97886128c4ac198d6e96c2f38dfa410fd388ab32
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-16 20:53:06 +0300

    Refactored and optimise code

---
+++ Git/Screens/FollowerListVC.swift
@@ -0,0 +1,56 @@
+//
+//  FollowerListVC.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 16/01/2021.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+    var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+}
