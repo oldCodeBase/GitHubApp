6051ee3b019ed74aa81001871cb936a137fa0da3
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-16 20:52:39 +0300

    Create custom AvatarImageView and added Errors enum

---
+++ Git/Utilities/GFError.swift
@@ -0,0 +1,15 @@
+//
+//  GFError.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 16/01/2021.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+}
