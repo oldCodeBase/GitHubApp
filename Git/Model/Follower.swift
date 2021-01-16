b05eba489a7d8041c76b234911c5b426505fc4b0
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-16 20:51:22 +0300

    Created Follower model

---
+++ Git/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  Git
+//
+//  Created by Ibragim Akaev on 16/01/2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    
+    var login: String
+    var avatarUrl: String
+
+}
