41d7237afd71c66dfecdea7a1a86c3f76040330f
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-21 17:11:36 +0300

    Created and add UserInfoHeaderVC

--- Git/Custom Views/Labels/GFBodyLabel.swift
+++ Git/Custom Views/Labels/GFBodyLabel.swift
@@ -27,7 +27,7 @@ class GFBodyLabel: UILabel {
 
     
     private func configure() {
-        textColor                                   = .label
+        textColor                                   = .secondaryLabel
         font                                        = UIFont.preferredFont(forTextStyle: .body)
         adjustsFontSizeToFitWidth                   = true
         minimumScaleFactor                          = 0.75
