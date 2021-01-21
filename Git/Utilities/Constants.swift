d4ad690d8f2d27346da174102892236c05c9eded
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-21 17:11:57 +0300

    Created Constants file

---
+++ Git/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitFollowers
+//
+//  Created by Ibragim Akaev on 21/01/2021.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
