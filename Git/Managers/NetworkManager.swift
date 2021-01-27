5d7ce46f59852c6ee0627996eece32fdfd45c85d
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-27 15:54:37 +0300

    Refactoring Network Manager Convenience and GFAlertVC

--- Git/Managers/NetworkManager.swift
+++ Git/Managers/NetworkManager.swift
@@ -41,9 +41,9 @@ class NetworkManager {
             }
             
             do {
-                let decoder = JSONDecoder()
+                let decoder                 = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let followers = try decoder.decode([Follower].self, from: data)
+                let followers               = try decoder.decode([Follower].self, from: data)
                 completed(.success(followers))
             } catch {
                 completed(.failure(.invalidData))
@@ -55,16 +55,14 @@ class NetworkManager {
     
     
     func getUserInfo(for username: String, completed: @escaping (Result<User, GFError>) -> Void) {
-        let endpoint = baseURL + "\(username)"
-        
-        guard let url = URL(string: endpoint) else {
+        let endpoint    = baseURL + "\(username)"
+        guard let url   = URL(string: endpoint) else {
             completed(.failure(.invalidUsername))
             return
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            if let _ = error {
+        let task        = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _    = error {
                 completed(.failure(.unableToComplete))
                 return
             }
@@ -80,13 +78,45 @@ class NetworkManager {
             }
             
             do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let user = try decoder.decode(User.self, from: data)
+                let decoder                     = JSONDecoder()
+                decoder.keyDecodingStrategy     = .convertFromSnakeCase
+                decoder.dateDecodingStrategy    = .iso8601
+                let user                        = try decoder.decode(User.self, from: data)
                 completed(.success(user))
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    
+    func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey    = NSString(string: urlString)
+        if let image    = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self,
+                  error == nil,
+                  let response = response as? HTTPURLResponse, response.statusCode == 200,
+                  let data = data,
+                  let image = UIImage(data: data) else {
+                    completed(nil)
+                    return
+                }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         
         task.resume()
